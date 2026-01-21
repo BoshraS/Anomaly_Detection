93db88cc69ec7b63b0f54853ad754cd0bd1ca965
Baran Aydogan <baran.aydogan@gmail.com>
2026-01-21 20:22:37 +0200

    sync

--- test/random/test.sh
+++ test/random/test.sh
@@ -4,8 +4,8 @@ echo "Running model test"
 
 anomap=../../build/anomap
 
-# inp=random_1K.vtk
-inp=random_100.vtk
+inp=random_1K.vtk
+# inp=random_100.vtk
 model=../../models/conv_autoencoder_scripted.pt
 
 ${anomap} modelTest \
